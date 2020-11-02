03aa2646817d234e6670575e41d2eeac963a10dc
Joanna-Joy Gray <jjgray21@hotmail.com>
2020-11-01 21:33:32 -0500

    JS for submit btn | fixed form colour

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+    var fbtn = document.querySelector('form button');
+    var msg = document.getElementsByClassName('message')[0];
+    fbtn.onclick = function(element){
+        element.preventDefault();
+        var email = document.querySelector('input').value;
+        if (email == ""){
+            // console.log("Please enter a valid email address.");
+            msg.innerHTML = "Please enter a valid email address.";
+            // console.log(msg.value);
+        }
+        else{
+            // console.log("Thank you! Your email address <" + email + "> has been added to our mailing list!");
+            msg.innerHTML = "Thank you! Your email address " + email + " has been added to our mailing list!";
+            // console.log(msg.value);
+        }
+        
+    }
+}
